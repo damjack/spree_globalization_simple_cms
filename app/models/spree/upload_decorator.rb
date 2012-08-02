cfb9807a20aac880e214e6e9d288eb389f857929
Riccardo Degan <riccardo.degan@diginess.it>
2012-08-02 15:25:00 +0200

    add attr_accessible

--- app/models/spree/upload_decorator.rb
+++ app/models/spree/upload_decorator.rb
@@ -3,4 +3,7 @@ Spree::Upload.class_eval do
   translates :attachment_name, :attachment_alt, :fallbacks_for_empty_translations => true
   globalize_accessors :locales => LANG_CMS, :attributes => [:attachment_name, :attachment_alt]
   
+  attr_accessible :translations_attributes
+  accepts_nested_attributes_for :translations
+  
 end
