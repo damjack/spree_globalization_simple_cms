6e0b0bb14cb970dbf9ccafbc138f170a903ee046
Riccardo Degan <riccardo.degan@diginess.it>
2012-08-03 15:02:44 +0200

    Fix _form.html.erb bug

--- app/models/spree/page_decorator.rb
+++ app/models/spree/page_decorator.rb
@@ -1,12 +1,15 @@
 Spree::Page.class_eval do
   LANG_CMS = Spree::Config[:locales].split(",").collect {|l| l.to_sym} rescue I18n.available_locales
   translates :name, :slug, :tag_title, :meta_description, :meta_keywords, :description, :content, :fallbacks_for_empty_translations => true
+  
   globalize_accessors :locales => LANG_CMS, :attributes => [:name, :slug, :tag_title, :meta_description, :meta_keywords, :description, :content]
   
-  attr_accessible :translations_attributes
+  attr_accessible :translations_attributes,  :name_attributes, :slug_attributes, :tag_title_attributes, :meta_description_attributes, :meta_keywords_attributes, :description_attributes, :content_attributes
   accepts_nested_attributes_for :translations
   
   extend ::FriendlyId
   friendly_id :name, :use => [:slugged, :globalize]
+  
+  
   
 end
--- app/models/spree/static_page_decorator.rb
+++ app/models/spree/static_page_decorator.rb
@@ -1,13 +1,6 @@
 Spree::StaticPage.class_eval do
-  
-  before_save :set_translates_slug
-  
-  def set_translates_slug
-    LANG.each do |locale|
-      I18n.with_locale(locale) do
-        self.slug = self.name.parameterize
-      end
-    end
-  end
+  LANG_CMS = Spree::Config[:locales].split(",").collect {|l| l.to_sym} rescue I18n.available_locales
+   translates :name, :slug, :tag_title, :meta_description, :meta_keywords, :description, :content, :fallbacks_for_empty_translations => true
+   globalize_accessors :locales => LANG_CMS, :attributes => [:name, :slug, :tag_title, :meta_description, :meta_keywords, :description, :content]
   
 end
